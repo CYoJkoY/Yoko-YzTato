48b65039989b48edcc15308b9dbdc64776f4fe42
CYoJkoY <Cjy3430525946@outlook.com>
2026-02-07 15:09:07 +0800

    Update enemy.gd

--- extensions/enemy.gd
+++ extensions/enemy.gd
@@ -15,28 +15,32 @@ func take_damage(value: int, args: TakeDamageArgs) -> Array:
 # =========================== Custom =========================== #
 func _yztato_extrusion_attack_ready() -> void:
     for player_index in RunData.get_player_count():
-        var extrusion_attack: int = RunData.get_player_effect(Utils.yztato_extrusion_attack_hash, player_index)
-        if extrusion_attack != 0:
-            NodeHurbox.collision_mask = Utils.ENEMIES_BIT + \
-                                        Utils.PLAYER_PROJECTILES_BIT + \
-                                        Utils.ENEMY_PROJECTILES_BIT + \
-                                        Utils.PET_PROJECTILES_BIT
+        if !RunData.get_player_effect_bool(Utils.yztato_extrusion_attack_hash, player_index): continue
+
+        NodeHurbox.collision_mask = Utils.ENEMIES_BIT + \
+                                    Utils.PLAYER_PROJECTILES_BIT + \
+                                    Utils.ENEMY_PROJECTILES_BIT + \
+                                    Utils.PET_PROJECTILES_BIT
 
 func _yztato_one_shot_loot_take_damage(args: TakeDamageArgs) -> void:
     if dead: return
+
+    if enemy_id == "evil_mob": return
     
-    if args.hitbox and \
-    args.hitbox.from and \
+    if args.hitbox and args.hitbox.from and \
     (
-        not (args.hitbox.from is Object) or \
+        !(args.hitbox.from is Object) or \
         (
             args.hitbox.from is Object and \
-            (not "player_index" in args.hitbox.from) or \
-            args.hitbox.from.player_index == -1
+                (
+                    !("player_index" in args.hitbox.from) or \
+                    args.hitbox.from.player_index == -1
+                )
         )
     
     ): return
+
+    if !args.hitbox or !is_instance_valid(args.hitbox) or !is_loot or \
+    !RunData.get_player_effect_bool(Utils.yztato_one_shot_loot_hash, args.hitbox.from.player_index): return
     
-    if (args.hitbox and is_instance_valid(args.hitbox) and is_loot and \
-    RunData.get_player_effect_bool(Utils.yztato_one_shot_loot_hash, args.hitbox.from.player_index)):
-        die()
+    die()
